c083f79a793847d6dad96e8b340d0fd97c69ddf9
MOHD ABDUL <RAHEEM.MOHDABDUL@thomsonreuters.com>
2018-10-03 20:37:16 +0530

    Add project files.

---
+++ Odd_bet50/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Odd_bet50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
